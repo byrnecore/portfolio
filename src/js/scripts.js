2760ea475eca18f1cfe6b9582f5106d4b2e81aa0
Shaun Byrne <byrnecore@gmail.com>
2018-04-24 22:20:28 +1000

    Added scroll reveal on main content divs

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -15,11 +15,16 @@ var swiper = new Swiper('.swiper-container', {
 var scroll = new SmoothScroll('a[href*="#"]');
 
 
+// ScrollReveal
+
+window.sr = ScrollReveal();
+// sr.reveal(document.querySelectorAll('.reveal', 0));
+sr.reveal('.reveal', { duration: 1000 });
+
 // GA tagging and tracing
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 window.dataLayer = window.dataLayer || [];
 function gtag(){dataLayer.push(arguments);}
 gtag('js', new Date());
-
 gtag('config', 'UA-117998943-1');	
